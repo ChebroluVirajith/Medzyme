ebd97e657e4c3c9fcffd35c520fa5fc63c25d898
ChebroluVirajith <virajithch4@gmail.com>
2026-03-11 10:17:47 +0000

    whiteee pageeeee

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// Notice the ./src/ below!
+import App from './src/App.jsx'  
+import './src/App.css'           
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
